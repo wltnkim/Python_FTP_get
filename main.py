9ef9fb4453c29a12a183b167de8b515be8c2731f
wltnkim <wltnkim>
2023-02-02 11:12:52 +0900

    second commit

--- main.py
+++ main.py
@@ -20,36 +20,35 @@ try:
     with ftplib.FTP() as ftp:
         ftp.connect(host=host,port=21)
         ftp.encoding = 'utf-8'
+        ftp.set_pasv(False)
         s = ftp.login(user=user,passwd=passwd)
         print("Connect success!")
 
-        #ftp.cwd('D:git/')  # 현재 폴더 이동
-        #print(ftp.pwd())
 
-        #filename = "main_.py"
-        #ftp.retrbinary("RETR " + filename, open(filename, 'wb').write)
 
         # List up files
         list = ftp.nlst()
-        print(list)
+        ftp.dir()
+        #print(list)
 
         # 파일다운로드
         
         for file in list :
             filename = file.split(sep='.')
             if not len(filename) >= 2:
-                print("Directory : " + file)
+                #print("Directory : " + file)
 
                 while (folder_tree > 0):
                     folder_tree = folder_tree - 1
                     ftp.cwd('..')
 
                 ftp.cwd(file)
+                ftp.dir()
                 folder_tree = folder_tree + 1
 
                 file_list = ftp.nlst()
                 for folder_tree_file in file_list:
-                    print("File : " + file + "/" + folder_tree_file)
+                    #print("File : " + file + "/" + folder_tree_file)
                     fd = open(folder_tree_file, 'wb')
                     ftp.retrbinary("RETR " + folder_tree_file, fd.write)
 
@@ -58,7 +57,7 @@ try:
                     folder_tree = folder_tree - 1
                     ftp.cwd('..')
 
-                print("File : " + file)
+                #print("File : " + file)
                 fd = open(file, 'wb')
                 ftp.retrbinary("RETR " + file, fd.write)
 
